3e9b386562a7f7e5ddcc7ab0895e4789b00833f9
ericheng0124 <233347237@qq.com>
2024-12-13 17:35:56 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '人力资源管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true, // 是否固定头部
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true // 是否显示侧边栏logo
+}
