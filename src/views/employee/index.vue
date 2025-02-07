dddfdfba24e2539f2239a23bcfe53d54432b7ffa
ericheng0124 <233347237@qq.com>
2025-02-07 17:38:30 +0800

    员工页面的表格结构和分页结构

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -23,7 +23,29 @@
           <el-button size="mini">excel导出</el-button>
         </el-row>
         <!-- 表格组件 -->
+        <el-table>
+          <el-table-column align="center" label="头像" />
+          <el-table-column label="姓名" />
+          <el-table-column label="手机号" sortable />
+          <el-table-column label="工号" sortable />
+          <el-table-column label="聘用形式" />
+          <el-table-column label="部门" />
+          <el-table-column label="入职时间" sortable />
+          <el-table-column align="center" label="操作" width="280px">
+            <template>
+              <el-button type="text" size="mini">查看</el-button>
+              <el-button type="text" size="mini">角色</el-button>
+              <el-button type="text" size="mini">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
         <!-- 分页 -->
+        <el-row style="height: 60px;" align="middle" type="flex" justify="end">
+          <el-pagination
+            layout="total, prev, pager, next"
+            :total="100"
+          />
+        </el-row>
       </div>
     </div>
   </div>
