27e54d942caabbf5117c6dd57f41288e99ec72b9
ericheng0124 <233347237@qq.com>
2025-02-10 11:38:48 +0800

    配置elementUI中文配置

--- src/main.js
+++ src/main.js
@@ -29,13 +29,13 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+// Vue.use(ElementUI, { locale })
+Vue.use(ElementUI)
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
-Vue.config.devtools = true
 new Vue({
   el: '#app',
   router,
