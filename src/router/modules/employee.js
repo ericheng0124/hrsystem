9a2221362791cfcd46608b8cdf469f55c56cea85
ericheng0124 <233347237@qq.com>
2025-02-11 16:06:00 +0800

    完成员工详情的数据和校验

--- src/router/modules/employee.js
+++ src/router/modules/employee.js
@@ -1,15 +1,23 @@
 import layout from '@/layout'
-
 export default {
   path: '/employee',
   component: layout,
   children: [{
-    name: 'employee',
     path: '',
+    name: 'employee',
     component: () => import('@/views/employee'),
     meta: {
       title: '员工',
       icon: 'people'
     }
+  }, {
+    path: '/employee/detail', // 员工详情的地址
+    component: () => import('@/views/employee/detail.vue'),
+    hidden: true, // 表示隐藏在左侧菜单
+    meta: {
+      title: '员工详情' // 显示在导航的文本
+    }
   }]
 }
+
+// 只想在左侧菜单显示一级路由，让二级路由只有一个显示在左侧菜单，就想不想显示的二级路由添加一个hidden属性，值为true
---
+++ src/views/employee/detail.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <div class="edit-form">
+        <el-form ref="userForm" label-width="220px" :rules="rules" :model="userInfo">
+          <!-- 姓名 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="姓名" prop="username">
+                <el-input v-model="userInfo.username" size="mini" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 手机 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="手机" prop="mobile">
+                <el-input
+                  v-model="userInfo.mobile"
+                  size="mini"
+                  class="inputW"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 工号 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="工号" prop="workNumber">
+                <!-- 工号是系统生成的 所以需要禁用 -->
+                <el-input v-model="userInfo.workNumber" size="mini" class="inputW" disabled />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 部门 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="部门" prop="departmentId">
+                <!-- 放置及联部门组件 -->
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 聘用形式 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="聘用形式" prop="formOfEmployment">
+                <el-select v-model="userInfo.formOfEmployment" size="mini" class="inputW">
+                  <el-option label="正式" :value="1" />
+                  <el-option label="非正式" :value="2" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 入职时间 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="入职时间" prop="timeOfEntry">
+                <el-date-picker
+                  v-model="userInfo.timeOfEntry"
+                  size="mini"
+                  type="date"
+                  value-format="yyyy-MM-dd"
+                  class="inputW"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 转正时间 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="转正时间" prop="correctionTime">
+                <el-date-picker
+                  v-model="userInfo.correctionTime"
+                  size="mini"
+                  type="date"
+                  class="inputW"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 员工照片 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="员工头像">
+                <!-- 放置上传图片 -->
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 保存个人信息 -->
+          <el-row type="flex">
+            <el-col :span="12" style="margin-left:220px">
+              <el-button size="mini" type="primary" @click="saveData">保存更新</el-button>
+            </el-col>
+          </el-row>
+        </el-form>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      userInfo: {
+        username: '', // 员工姓名
+        workNumber: '', // 员工工号
+        mobile: '', // 员工手机号
+        departmentId: '', // 员工部门id
+        formOfEmployment: '', // 聘用形式
+        timeOfEntry: '', // 入职时间
+        correctionTime: '' // 转正时间
+      },
+      rules: {
+        // 员工姓名效验规则
+        username: [
+          { required: true, message: '请输入员工姓名', trigger: 'blur' },
+          { min: 2, max: 6, message: '长度在 2 到 6 个字符', trigger: 'blur' }
+        ],
+        // 员工手机号效验规则
+        mobile: [
+          { required: true, message: '请输入员工手机号', trigger: 'blur' },
+          { pattern: '/^1[3-9]\d{9}$/', message: '手机号格式不正确', trigger: 'blur' }
+        ],
+        // 员工部门id效验规则
+        departmentId: [{ required: true, message: '请选择员工所属部门', trigger: 'blur' }],
+        // 聘用形式效验规则
+        formOfEmployment: [{ required: true, message: '请选择员工所属聘用形式', trigger: 'blur' }],
+        // 入职时间效验规则
+        timeOfEntry: [{ required: true, message: '请选择员工入职时间', trigger: 'blur' }],
+        // 转正时间效验规则
+        correctionTime: [
+          { required: true, message: '请选择员工转正时间', trigger: 'blur' },
+          {
+            validator: (rule, value, callback) => {
+              if (this.userInfo.timeOfEntry) {
+                if (new Date(this.userInfo.timeOfEntry) > new Date(value)) {
+                  callback(new Error('转正时间不能早于入职时间'))
+                  return
+                }
+              }
+              callback()
+            }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    saveData() {
+      this.$refs.userForm.validate()
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    .edit-form {
+      background: #fff;
+      padding: 20px;
+      .inputW {
+        width: 380px
+      }
+    }
+
+</style>
